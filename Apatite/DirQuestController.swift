d5714a119f5e806c1e4466e71f2e475eb31ce817
JonathanAndrei12 <jonathanandreiwibawa@gmail.com>
2021-04-05 17:42:57 +0700

    Updte mapss

---
+++ Apatite/DirQuestController.swift
@@ -0,0 +1,20 @@
+//
+//  DirQuestController.swift
+//  Apatite
+//
+//  Created by Jonathan Andrei on 05/04/21.
+//
+
+import UIKit
+
+class DirectoryQuestController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func modalClose(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Apatite/MapsController.swift
@@ -0,0 +1,23 @@
+//
+//  MapsController.swift
+//  Apatite
+//
+//  Created by Jonathan Andrei on 05/04/21.
+//
+
+import UIKit
+
+class MapsViewController: UIViewController {
+    
+    @IBOutlet weak var estMinLabel: UILabel!
+    @IBOutlet weak var rangeLabel: UILabel!
+    @IBOutlet weak var arrTimeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+}
